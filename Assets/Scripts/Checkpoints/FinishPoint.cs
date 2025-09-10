4ab210bda92298d4804950bb452d990af69e7ecb
nadilHesara <nadilhesara@gmail.com>
2025-09-10 18:48:03 +0530

    Startpoint and Endpoint animations and triggers are done. Respawn point synced with animation.

---
+++ Assets/Scripts/Checkpoints/FinishPoint.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishPoint : MonoBehaviour
+{
+
+    private Animator anim => GetComponent<Animator>();
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Player player = collision.GetComponent<Player>();
+
+        if (player != null)
+        {
+            anim.SetTrigger("activate");
+            Debug.Log("Level Completed!");
+
+
+        }
+    }
+}
---
+++ Assets/Scripts/Checkpoints/Startpoint.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Startpoint : MonoBehaviour
+{
+    private Animator anim => GetComponent<Animator>();
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Player player = collision.GetComponent<Player>();
+
+        if (player != null)
+        {
+            anim.SetTrigger("activate");
+           
+
+
+        }
+    }
+}
