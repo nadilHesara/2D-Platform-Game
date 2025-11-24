7f3f7b47080f411781b977b6e22116c797a19ef1
nadilHesara <nadilhesara@gmail.com>
2025-11-24 16:29:28 +0530

    Enemy Mushroom Created

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Windows;
+
+public class Enemy : MonoBehaviour
+{
+    protected Animator anim;
+    protected Rigidbody2D rb;
+
+    [SerializeField] protected float moveSpeed;
+    [SerializeField] protected float idleDuration;
+    protected float idleTimer;
+
+
+    [Header("Basic Collision")]
+    [SerializeField] protected float groundCheckDistance = 1.1f;
+    [SerializeField] protected float wallCheckDistance = 0.7f;
+    [SerializeField] protected LayerMask whatIsGround;
+    [SerializeField] protected Transform groundCheck;
+    protected bool isGrounded;
+    protected bool isWallDetected;
+    protected bool isGroundInfrontDetected;
+
+
+    protected int facingDir = -1;
+    protected bool facingRight = false;
+
+    protected virtual void Awake()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    protected virtual void Update()
+    {
+        idleTimer -= Time.deltaTime;
+    }
+    protected virtual void HandleFlip(float xValue)
+    {
+        if (xValue < 0 && facingRight || xValue > 0 && !facingRight)
+        {
+            Flip();
+        }
+    }
+
+    protected virtual void Flip()
+    {
+        facingDir = facingDir * -1;
+        transform.Rotate(0, 180, 0);
+        facingRight = !facingRight;
+
+    }
+
+    protected virtual void HandleCollisions()
+    {
+        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, whatIsGround);
+        isGroundInfrontDetected = Physics2D.Raycast(groundCheck.position, Vector2.down, groundCheckDistance, whatIsGround);
+        isWallDetected = Physics2D.Raycast(transform.position, Vector2.right * facingDir, wallCheckDistance, whatIsGround);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position, new Vector2(transform.position.x, transform.position.y - groundCheckDistance));
+        Gizmos.DrawLine(groundCheck.position, new Vector2(groundCheck.position.x, groundCheck.position.y - groundCheckDistance));
+        Gizmos.DrawLine(transform.position, new Vector2(transform.position.x + (wallCheckDistance * facingDir), transform.position.y));
+    }
+
+
+
+}
---
+++ Assets/Scripts/Enemy_Mushroom.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Mushroom : Enemy
+{
+   
+
+    protected override void Update()
+    {
+        base.Update();
+
+        anim.SetFloat("xVelocity", rb.velocity.x);
+        HandleCollisions();
+        HandleMovement();
+
+        if(isGrounded)
+            HandleTurnAround();
+    }
+
+    private void HandleTurnAround()
+    {
+        if (!isGroundInfrontDetected || isWallDetected)
+        {
+
+
+            Flip();
+            idleTimer = idleDuration;
+            rb.velocity = Vector2.zero;
+
+        }
+    }
+
+    private void HandleMovement()
+    {
+        if (idleTimer > 0)
+            return;
+
+        if(isGroundInfrontDetected)
+            rb.velocity = new Vector2(moveSpeed*facingDir, rb.velocity.y);
+    }
+
+
+}
