97969aa3fed93216e6ad983d73d488f3e5229f57
nadilHesara <nadilhesara@gmail.com>
2025-12-10 13:14:43 +0530

    Added some UI elements to the game. Settings, Skin Selection, Level Selection, Main menu

--- Assets/Scripts/UI_MainMenu.cs
+++ Assets/Scripts/UI_MainMenu.cs
@@ -6,6 +6,8 @@ public class UI_MainMenu : MonoBehaviour
     private UI_FadeEffect fadeEffect;
     public string sceneName;
 
+    [SerializeField] private GameObject[] uiElements;
+
     private void Awake()
     {
         fadeEffect = GetComponentInChildren<UI_FadeEffect>();
@@ -15,6 +17,16 @@ public class UI_MainMenu : MonoBehaviour
     private void Start()
     {
         fadeEffect.ScreenFade(0, 1.5f);
+    }
+
+    public void SwitchUI(GameObject uiToEnable)
+    {
+        foreach (GameObject ui in uiElements)
+        {
+            ui.SetActive(false);
+        }
+
+        uiToEnable.SetActive(true);
     }
 
     public void NewGame()
